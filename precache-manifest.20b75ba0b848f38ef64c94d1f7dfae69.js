12d6874bef79ca34f9c217af8ef5cebd88b686fc
Reshi <juancdominici@gmail.com>
2020-10-05 17:10:17 -0300

    Updates

---
+++ precache-manifest.20b75ba0b848f38ef64c94d1f7dfae69.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c81296b4087d5de67bb0e0afb176b66",
+    "url": "/markdown-previewer/index.html"
+  },
+  {
+    "revision": "448a003187f639be4e9c",
+    "url": "/markdown-previewer/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "1dee3d9f034ad2562b5a",
+    "url": "/markdown-previewer/static/css/main.e890d5df.chunk.css"
+  },
+  {
+    "revision": "448a003187f639be4e9c",
+    "url": "/markdown-previewer/static/js/2.16522698.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/markdown-previewer/static/js/2.16522698.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1dee3d9f034ad2562b5a",
+    "url": "/markdown-previewer/static/js/main.25fa6805.chunk.js"
+  },
+  {
+    "revision": "380a9cf99d31db8da525",
+    "url": "/markdown-previewer/static/js/runtime-main.4fd0b226.js"
+  }
+]);
